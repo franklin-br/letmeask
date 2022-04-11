f8f3b49676624c0696652869bb6ac4b0a819707a
dev-franklin <91273045+dev-franklin@users.noreply.github.com>
2022-04-12 00:47:35 +0100

    Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { Button } from './components/Button'
+
+import { Home } from "./pages/Home";
 
 function App() {
   return (
-    <div>
-      <h1>hello world</h1>
-      <Button />
-      <Button />
-      <Button />
-    </div>
+    <Home />
   );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,39 @@
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
+
+// setpack {snowpack, vite, ...}
+
+// Module Bundler -> 
+
+export function Home() {
+    return (
+        <div>
+            <aside>
+                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+                <strong>Crie salas de Q&amp;A ao-vivo</strong>
+                <p>Tire as dúvidas da sua audiência em Tempo-real</p>
+            </aside>
+            <main>
+                <div>
+                    <img src={logoImg} alt="Letmeask" />
+
+                    <button>
+                        <img src={googleIconImg} alt="Logo do Google" />
+                        Crie sua sala com Google
+                    </button>
+                    <div>Ou entre em uma sala</div>
+                    <form>
+                        <input
+                            type="text"
+                            placeholder='Digíte o código da sala'
+                        />
+                        <button type="submit">
+                            Entra na sala
+                        </button>
+                    </form>
+                </div>
+            </main>
+        </div>
+    )
+}
