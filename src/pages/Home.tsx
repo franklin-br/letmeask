b4dfd02a300ec0652b76550e448ea7c8822b4988
dev-franklin <91273045+dev-franklin@users.noreply.github.com>
2022-04-15 01:17:44 +0100

    style v1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,20 +2,22 @@ import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
+import '../style/auth.scss';
+
 // setpack {snowpack, vite, ...}
 
 // Module Bundler -> 
 
 export function Home() {
     return (
-        <div>
+        <div id="page-auth">
             <aside>
                 <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
                 <strong>Crie salas de Q&amp;A ao-vivo</strong>
                 <p>Tire as dúvidas da sua audiência em Tempo-real</p>
             </aside>
             <main>
-                <div>
+                <div className='main-content'>
                     <img src={logoImg} alt="Letmeask" />
 
                     <button>
